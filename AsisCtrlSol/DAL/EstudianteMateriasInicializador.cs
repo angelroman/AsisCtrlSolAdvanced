e4ba115d5caca870ffb2a8ed51304890641c3c06
angelvstbmf <angelvstbmf192@gmail.com>
2015-12-09 00:43:44 -0600

    Update EstudianteMateriasInicializador.cs

--- AsisCtrlSol/DAL/EstudianteMateriasInicializador.cs
+++ AsisCtrlSol/DAL/EstudianteMateriasInicializador.cs
@@ -12,17 +12,31 @@ namespace AsisCtrlSol.DAL
     {
         protected override void Seed(EstudianteMateriasContexto context)
         {
-            var Estudiante = new Estudiante{EstudianteID=10100282, Nombre="Luis",Apellido_Materno="Romero",Apellido_Paterno="Reyes",Fecha_Nacimiento="08/04/91"};
-            context.Estudiantes.Add(Estudiante);
+            var Estudiante = new List<Estudiante>
+            {
+                new Estudiante{ EstudianteID =10100282, Nombre="Luis",Apellido_Materno="Romero",Apellido_Paterno="Reyes",Fecha_Nacimiento="08/04/91"},
+                new Estudiante { EstudianteID =10100283, Nombre="Angel",Apellido_Materno="Roman",Apellido_Paterno="Camacho",Fecha_Nacimiento="08/04/91"}
+            };
+            Estudiante.ForEach(u => context.Estudiantes.Add(u));
+            context.SaveChanges();
             
 
-            var Materia = new Materia { Nombre_Materia = "Ingles", Creditos_Materia = 5, ID = 1 };
-            context.Materias.Add(Materia);
-
-            var Enlistar = new Enlistamiento { EnlistamientoID = 08122015, EstudianteID = 10100282, MateriaID = 1 };
-
+            var Materia = new List<Materia>
+            {
+                new Materia{ Nombre_Materia = "Ingles", Creditos_Materia = 5, ID = 1 },
+                new Materia{ Nombre_Materia = "Fisica", Creditos_Materia = 4, ID = 2 },
+                new Materia{ Nombre_Materia = "Quimica", Creditos_Materia = 5, ID = 3 }
+            };
+            Materia.ForEach(m => context.Materias.Add(m));
             context.SaveChanges();
 
+            var Enlistar = new List<Enlistamiento>
+            {
+                new Enlistamiento { EnlistamientoID = 08122015, EstudianteID = 10100282, MateriaID = 1 },
+                new Enlistamiento { EnlistamientoID = 08122016, EstudianteID = 10100283, MateriaID = 1 },
+            };
+
+            context.SaveChanges();
 
         }
     }
